ef83c37ea1986e22e96eb73bb1b1f7233ef75ec1
dahnepfl <dahn.youssefi@epfl.ch>
2024-05-03 08:15:22 +0200

    up

--- projects/custom-display/src/lib/service/dy-contact/dy-contact.service.ts
+++ projects/custom-display/src/lib/service/dy-contact/dy-contact.service.ts
@@ -14,21 +14,34 @@ export class DyContactService {
     email: new FormControl('', [Validators.required, Validators.email]),
     message: new FormControl('', Validators.required)
   });
+  formDataLong: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    nom: new FormControl('', Validators.required),
+    lastName: new FormControl(''),
+    phone: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', Validators.required)
+  });
   private readonly formLoadedTimestamp: number;
-
 
   constructor(private http: HttpClient, @Inject('backendUrl') private backendUrl: string) {
     this.formLoadedTimestamp = Date.now();
   }
 
   sendContactForm(): Observable<any> {
-    if (this.formData.invalid || (Date.now() - this.formLoadedTimestamp) < 5000 || this.formData.getRawValue().lastName) {
+    return this.send(this.formData)
+  }
+  sendContactFormLong(): Observable<any> {
+    return this.send(this.formDataLong)
+  }
+
+  private send(form: FormGroup) : Observable<any> {
+    if (form.invalid || (Date.now() - this.formLoadedTimestamp) < 5000 || form.getRawValue().lastName) {
       return throwError(() => 'form Invalid');
     }
     LoadingService.startLoading()
-    return this.http.get(this.backendUrl + '/contact', {params: this.formData.getRawValue()}).pipe(finalize(() => {
+    return this.http.get(this.backendUrl + '/contact', {params: form.getRawValue()}).pipe(finalize(() => {
       LoadingService.stopLoading()
     }));
   }
-
 }
