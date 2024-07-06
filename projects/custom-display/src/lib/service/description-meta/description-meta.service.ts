b1cd15795e2f169843db772244da6e43ab6f6140
Dahn Y <dahn.youssefi@epfl.ch>
2024-07-06 11:55:14 +0200

    ok

--- projects/custom-display/src/lib/service/description-meta/description-meta.service.ts
+++ projects/custom-display/src/lib/service/description-meta/description-meta.service.ts
@@ -1,6 +1,5 @@
 import {ElementRef, Inject, Injectable, PLATFORM_ID, Renderer2} from '@angular/core';
 import {Meta} from "@angular/platform-browser";
-import {DOCUMENT, isPlatformBrowser} from "@angular/common";
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +7,6 @@ import {DOCUMENT, isPlatformBrowser} from "@angular/common";
 export class DescriptionMetaService {
 
   constructor(
-    @Inject(PLATFORM_ID) private _platformId: Object,
     private meta: Meta) {
   }
 
@@ -16,15 +14,12 @@ export class DescriptionMetaService {
                renderer2: Renderer2,
                elementRef: ElementRef
                ) : void {
-    if (isPlatformBrowser(this._platformId)) {
-
-      let script = renderer2.createElement('script');
-      script.type = `application/ld+json`;
-      script.text = text;
+    let script = renderer2.createElement('script');
+    script.type = `application/ld+json`;
+    script.text = text;
 
 
-      renderer2.appendChild(elementRef.nativeElement, script);
-    }
+    renderer2.appendChild(elementRef.nativeElement, script);
   }
 
   updateDescription(newDescription: string): void {
